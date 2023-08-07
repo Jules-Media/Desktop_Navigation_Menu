86b72b5a7ebf087bf202ad196f9a7039d10b25b5
Julian Schumacher <professional@julianschumacher.dev>
2023-08-07 13:34:13 +0200

    Update Dart and Flutter Version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.6
+
+* Update Dart and Flutter Version
+
 ## 0.1.5
 
 * Bugfix
--- test/desktop_navigation_menu_test.dart
+++ test/desktop_navigation_menu_test.dart
@@ -1,5 +1,1 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:desktop_navigation_menu/desktop_navigation_menu.dart';
-
 void main() {}
